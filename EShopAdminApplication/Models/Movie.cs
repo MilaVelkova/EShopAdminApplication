31ed46f2346ffe0afed0766b9fadfcadaa1cdf36
MilaVelkova <milavelkova02@gmail.com>
2024-04-29 17:46:20 +0200

    Add project files.

---
+++ EShopAdminApplication/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShopAdminApplication.Models
+{
+    public class Movie
+    {
+        [Required]
+        public string MovieName { get; set; }
+        [Required]
+        public string MovieDescription { get; set; }
+        [Required]
+        public string MovieImage { get; set; }
+        [Required]
+        public double Rating { get; set; }
+    }
+}
---
+++ EShopAdminApplication/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShopAdminApplication.Models
+{
+    public class Ticket
+    {
+
+        public Guid MovieId { get; set; }
+        public Movie? Movie { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+        public virtual EShopApplicationUser? CreatedBy { get; set; }
+    }
+}
